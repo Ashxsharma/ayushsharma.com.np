f7106be6e2a209210760ecdf3d213d02ff10e7f1
ayush sharma <sharmaashx18@gmail.com>
2025-06-03 19:37:25 +0545

    Implement responsive navigation menu with toggle functionality for mobile

--- jsfiles/portofolio.js
+++ jsfiles/portofolio.js
@@ -29,3 +29,11 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+/* TOGGLING FOR MOBIL BUNPAUROTI */
+  const toggle = document.getElementById('menu-toggle');
+  const menu = document.getElementById('main-menu');
+
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+  });
+
